62d0b96cd8cc9cdb392a02a3dff74f65b3d67961
Usuario <Usuario@CPD03-M06>
2024-04-29 15:24:03 -0300

    Adicionar arquivos de projeto.

---
+++ MauiFlix/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFlix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
